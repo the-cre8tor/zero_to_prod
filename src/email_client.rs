7923d11e706e39ee4e564a025acf586d307a5e74
Alexander Nitiola <anitiola@aellacredit.com>
2024-10-05 01:08:57 +0100

    Refactoring: Avoid Unnecessary Memory Allocations

--- src/email_client.rs
+++ src/email_client.rs
@@ -4,12 +4,13 @@ use reqwest::Client;
 use crate::domain::SubscriberEmail;
 
 #[derive(serde::Serialize)]
-struct SendEmailRequest {
-    from: String,
-    to: String,
-    subject: String,
-    html_body: String,
-    text_body: String,
+#[serde(rename_all = "PascalCase")]
+struct SendEmailRequest<'a> {
+    from: &'a str,
+    to: &'a str,
+    subject: &'a str,
+    html_body: &'a str,
+    text_body: &'a str,
 }
 
 pub struct EmailClient {
@@ -43,11 +44,11 @@ impl EmailClient {
         let url = format!("{}/email", self.base_url);
 
         let request_body = SendEmailRequest {
-            from: self.sender.as_ref().to_owned(),
-            to: recipient.as_ref().to_owned(),
-            subject: subject.to_owned(),
-            html_body: html_content.to_owned(),
-            text_body: text_content.to_owned(),
+            from: self.sender.as_ref(),
+            to: recipient.as_ref(),
+            subject,
+            html_body: html_content,
+            text_body: text_content,
         };
 
         self.http_client
@@ -72,8 +73,30 @@ mod tests {
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker};
     use redact::Secret;
+    use serde_json::{from_slice, Value};
+
     use wiremock::matchers::{header, header_exists, method, path};
-    use wiremock::{Mock, MockServer, ResponseTemplate};
+    use wiremock::{Match, Mock, MockServer, Request, ResponseTemplate};
+
+    struct SendEmailBodyMatcher;
+
+    impl Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            let result: Result<Value, _> = from_slice(&request.body);
+
+            if let Ok(body) = result {
+                dbg!(&body);
+
+                body.get("From").is_some()
+                    && body.get("To").is_some()
+                    && body.get("Subject").is_some()
+                    && body.get("HtmlBody").is_some()
+                    && body.get("TextBody").is_some()
+            } else {
+                false
+            }
+        }
+    }
 
     #[tokio::test]
     async fn send_email_fires_a_request_to_base_url() {
@@ -86,6 +109,7 @@ mod tests {
             .and(header("Content-Type", "application/json"))
             .and(path("/email"))
             .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1) // should receive exactly one request that matches the conditions set by this mock.
             .mount(&mock_server)
