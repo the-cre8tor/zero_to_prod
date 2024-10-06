afb171ee2871f1523b34288a0ecdc32b8b8869ff
Alexander Nitiola <anitiola@aellacredit.com>
2024-10-06 02:07:53 +0100

    refactored the subscriber api

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -37,20 +37,37 @@ pub async fn subscribe(
         return HttpResponse::InternalServerError().finish();
     };
 
-    let email_response = email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            "Welcome to our newsletter!",
-            "Welcome to our newsletter!",
-        )
-        .await;
-
-    if email_response.is_err() {
+    if send_email(&email_client, new_subscriber).await.is_err() {
         return HttpResponse::InternalServerError().finish();
     }
 
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+async fn send_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://there-is-no-such-domain.com/subscriptions/confirm";
+
+    let html_email = format!(
+        "Welcome to our newsletter!<br />\
+        Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+
+    let text_email = format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+
+    email_client
+        .send_email(new_subscriber.email, "Welcome!", &html_email, &text_email)
+        .await
 }
 
 #[tracing::instrument(
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,5 +1,7 @@
 //! tests/api/subscriptions.rs
 
+use linkify::{LinkFinder, LinkKind};
+use serde_json::Value;
 use wiremock::{
     matchers::{method, path},
     Mock, ResponseTemplate,
@@ -97,7 +99,45 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
         .await;
 
     // Act
+    let response = app.post_subscriptions(body.into()).await;
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    // Arrange
+    let app = TestApp::spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    // Act
     app.post_subscriptions(body.into()).await;
 
     // Assert
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    // Extract the link from one of the request fields.
+    let get_link = |input: &str| {
+        let links: Vec<_> = LinkFinder::new()
+            .links(input)
+            .filter(|value| *value.kind() == LinkKind::Url)
+            .collect();
+
+        assert_eq!(links.len(), 1);
+
+        links[0].as_str().to_owned()
+    };
+
+    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(&body["TextBody"].as_str().unwrap());
+
+    assert_eq!(html_link, text_link);
 }
