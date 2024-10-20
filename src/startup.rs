6de652c2f6f9737ddbe1ea70344c9e0d266ed89b
Alexander Nitiola <anitiola@aellacredit.com>
2024-10-20 09:17:05 +0100

    setup the basic composition for the newsletters api

--- src/startup.rs
+++ src/startup.rs
@@ -9,7 +9,7 @@ use tracing_actix_web::TracingLogger; // Transmission Control Protocol: [TCP]
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, subscribe};
+use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
 
 // NOTE: HTTP & TCP is a protocol
 
@@ -71,6 +71,7 @@ impl Application {
                 .route("/health-check", get().to(health_check))
                 .route("/subscriptions", post().to(subscribe))
                 .route("/subscriptions/confirm", get().to(confirm))
+                .route("/newsletters", post().to(publish_newsletter))
                 .app_data(db_pool.clone())
                 .app_data(email_client.clone())
                 .app_data(base_url.clone())
