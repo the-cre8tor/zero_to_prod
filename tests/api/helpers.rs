dc5c2ad87066958d2075af10f6a5eccbdd2d6c5d
Alexander Nitiola <anitiola@aellacredit.com>
2024-10-25 11:40:11 +0100

    timing attack best practices

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,6 @@
 //! tests/api/helpers.rs
 
-use argon2::{password_hash::SaltString, Argon2, PasswordHasher};
+use argon2::{password_hash::SaltString, Algorithm, Argon2, Params, PasswordHasher, Version};
 use linkify::{LinkFinder, LinkKind};
 use redact::Secret;
 use reqwest::{Client, Response, Url};
@@ -46,10 +46,14 @@ impl TestUser {
     async fn store(&self, pool: &PgPool) {
         let salt = SaltString::generate(&mut rand::thread_rng());
 
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
 
         sqlx::query!(
             r#"
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,6 +1,7 @@
 //! tests/api/newsletter.rs
 
 use crate::helpers::{ConfirmationLinks, TestApp};
+use uuid::Uuid;
 use wiremock::matchers::{any, method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -148,6 +149,76 @@ async fn requests_missing_authorization_are_rejected() {
     // Act
     let response = reqwest::Client::new()
         .post(&format!("{}/newsletters", &app.address))
+        .json(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
+#[tokio::test]
+async fn non_existing_user_is_rejected() {
+    // Arrange
+    let app = TestApp::spawn_app().await;
+
+    // Random credentials
+    let username = Uuid::new_v4().to_string();
+    let password = Uuid::new_v4().to_string();
+
+    let url = &format!("{}/newsletters", &app.address);
+    let body = &serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(url)
+        .basic_auth(username, Some(password))
+        .json(body)
+        .send()
+        .await
+        .expect("failed to execute request");
+
+    // Assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    )
+}
+
+#[tokio::test]
+async fn invalid_password_is_rejected() {
+    // Arrange
+    let app = TestApp::spawn_app().await;
+    let username = &app.test_user.username;
+    let password = Uuid::new_v4().to_string();
+
+    assert_ne!(app.test_user.password, password);
+
+    let url = &format!("{}/newsletters", &app.address);
+    let body = &serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(url)
+        .basic_auth(username, Some(password))
         .json(body)
         .send()
         .await
