f0954b6f3ab04dedf260bc22260640803f7798d7
Alexander Nitiola <anitiola@aellacredit.com>
2024-10-25 16:51:07 +0100

    Rejecting Invalid Credentials

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,17 @@
-use actix_web::{http::header::LOCATION, web::Form, HttpResponse};
+use std::fmt::Debug;
+
+use actix_web::{
+    http::{header::LOCATION, StatusCode},
+    web::{Data, Form},
+    HttpResponse, ResponseError,
+};
 use redact::Secret;
+use sqlx::PgPool;
+
+use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    routes::error_chain_fmt,
+};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -7,8 +19,51 @@ pub struct FormData {
     password: Secret<String>,
 }
 
-pub async fn login(_form: Form<FormData>) -> HttpResponse {
-    HttpResponse::SeeOther()
+#[tracing::instrument(
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(form: Form<FormData>, pool: Data<PgPool>) -> Result<HttpResponse, LoginError> {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+
+    tracing::Span::current().record("username", tracing::field::display(&credentials.username));
+
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|error| match error {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(error.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(error.into()),
+        })?;
+
+    tracing::Span::current().record("user_id", tracing::field::display(&user_id));
+
+    Ok(HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
-        .finish()
+        .finish())
+}
+
+#[derive(thiserror::Error)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl Debug for LoginError {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, formatter)
+    }
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
