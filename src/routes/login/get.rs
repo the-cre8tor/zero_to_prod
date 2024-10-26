0d59cb45dc9b7cf32e652ccdbe0b26aa3a913183
Alexander Nitiola <anitiola@aellacredit.com>
2024-10-26 21:34:51 +0100

    How To Read A Cookie In actix-web

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,51 +1,10 @@
-use actix_web::{
-    http::header::ContentType,
-    web::{Data, Query},
-    HttpResponse,
-};
-use hmac::{Hmac, Mac};
+use actix_web::{http::header::ContentType, HttpRequest, HttpResponse};
 
-use crate::startup::HmacSecret;
-
-#[derive(serde::Deserialize)]
-pub struct QueryParams {
-    error: String,
-    tag: String,
-}
-
-impl QueryParams {
-    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
-        let tag = hex::decode(self.tag)?;
-        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
-
-        let mut mac =
-            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
-        mac.update(query_string.as_bytes());
-        mac.verify_slice(&tag)?;
-
-        Ok(self.error)
-    }
-}
-
-pub async fn login_form(
-    query: Option<Query<QueryParams>>,
-    secret: Data<HmacSecret>,
-) -> HttpResponse {
-    let error_html = match query {
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error) => {
-                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
-            }
-            Err(error) => {
-                tracing::warn!(
-                    error.message = %error,
-                    error.cause_chain = ?error,
-                    "Failed to verify query parameters using the HMAC tag"
-                );
-
-                "".into()
-            }
-        },
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
+        }
         None => "".into(),
     };
 
