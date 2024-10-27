fc03f68aaf1f9e4999d327dd300c155275930024
Alexander Nitiola <anitiola@aellacredit.com>
2024-10-27 12:33:10 +0100

    10.8.2.3 Unhappy Path: The Current Password Is Invalid

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,9 +1,15 @@
 //! src/routes/admin/password/post.rs
-use actix_web::{web, HttpResponse};
+use actix_web::{
+    web::{self, Data},
+    HttpResponse,
+};
 use actix_web_flash_messages::FlashMessage;
 use redact::Secret;
+use sqlx::PgPool;
 
 use crate::{
+    authentication::{validate_credentials, AuthError, Credentials},
+    routes::admin::dashboard::get_username,
     session_state::TypedSession,
     utils::{error_500, see_other},
 };
@@ -18,10 +24,15 @@ pub struct FormData {
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
+    pool: Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(error_500)?.is_none() {
+    let user_id = session.get_user_id().map_err(error_500)?;
+
+    if user_id.is_none() {
         return Ok(see_other("/login"));
-    };
+    }
+
+    let user_id = user_id.unwrap();
 
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
@@ -30,6 +41,23 @@ pub async fn change_password(
         .send();
 
         return Ok(see_other("/admin/password"));
+    }
+
+    let username = get_username(user_id, &pool).await.map_err(error_500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+
+    if let Err(error) = validate_credentials(credentials, &pool).await {
+        return match error {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(error_500(error).into()),
+        };
     }
 
     todo!()
