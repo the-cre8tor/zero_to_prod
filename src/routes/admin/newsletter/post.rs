eda82ca6d2a8ebf32c6f409295374cc787600a9d
Alexander Nitiola <anitiola@aellacredit.com>
2024-11-02 22:44:27 +0100

    11.9.2 Synchronization

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -1,7 +1,7 @@
 use crate::authentication::UserId;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
-use crate::idempotency::{get_saved_response, save_response, IdempotencyKey};
+use crate::idempotency::{save_response, try_processing, IdempotencyKey, NextAction};
 use crate::utils::{error_400, error_500, see_other};
 use actix_web::web::ReqData;
 use actix_web::{web, HttpResponse};
@@ -38,12 +38,15 @@ pub async fn publish_newsletter(
     let user_id = user_id.into_inner();
     let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(error_400)?;
 
-    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
+    match try_processing(&pool, &idempotency_key, *user_id)
         .await
         .map_err(error_500)?
     {
-        FlashMessage::info("The newsletter issue has been published!").send();
-        return Ok(saved_response);
+        NextAction::StartProcessing => {}
+        NextAction::ReturnSavedResponse(saved_response) => {
+            FlashMessage::info("The newsletter issue has been published!").send();
+            return Ok(saved_response);
+        }
     }
 
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(error_500)?;
